278cbd5a2dfa861856a1dddc224200a7f9f54bdf
aedda <44825280+68110923@users.noreply.github.com>
2020-04-25 18:11:56 +0800

    修改

--- mallProject/mallProject/settings.py
+++ mallProject/mallProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!l(!w0!vxf*g+p6v9ws+31k)+x&0dmfg5jd+5-fyko+e^g$2ii'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
